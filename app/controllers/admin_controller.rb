bacf87d667a9bd7aa55b9557236fb7b041fdffa9
Alex McRitchie <amcritchie@gmail.com>
2015-07-29 21:21:05 -0600

    showing in progess and finished games

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,8 +13,8 @@ class AdminController < ApplicationController
 
       @pvp_matches = Match.where(['away_user_id > ?', 10])
 
-      # @in_progress_matches = Match.where(match_status: 'in progress').order('created_at DESC')
-      # @finished_matches = Match.where(match_status: 'finished').order('created_at DESC')
+      @in_progress_matches = Match.where(match_status: 'in progress').order('created_at DESC')
+      @finished_matches = Match.where(match_status: 'finished').order('created_at DESC')
       # @new_matches = Match.where(match_status: 'new').order('created_at DESC')
       # @pending_matches = Match.where(match_status: 'pending').order('created_at DESC')
     end
